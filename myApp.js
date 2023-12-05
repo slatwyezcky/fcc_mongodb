1e32dd247c92e1cb5adf5d8a9253860651e3c9f4
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-05 17:10:22 +0600

    implement createAndSavePerson

--- myApp.js
+++ myApp.js
@@ -18,7 +18,15 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: 'John',
+    age: 25,
+    favoriteFoods: ['Eggs'],
+  });
+  person.save(function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
